82fc9a7f2a38fb1ccbe465cfc8cadbdb6e88ac8d
z21ite11 <z21ite11@z00p3-10.SPSKNM>
2021-09-30 14:33:05 +0200

    kjk

---
+++ main.py
@@ -0,0 +1,18 @@
+bohatý = input("Si bohatý?")
+šťastný = input("Si šťastný?")
+
+if šťastný == "Áno":
+    if bohatý == "Áno":
+        print("Gratulujem")
+    else:
+        print("Skús sa viac usmievať")
+else:
+    if bohatý == "Áno":
+        print("skus sa viac usmievať")
+    else:
+        print("Sorry je mi ta luto")
+
+
+
+
+
