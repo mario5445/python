d25618793a59c258ed921a2e3ae161f5a6cc2ed4
mario5445 <mario.lastovica228@gmail.com>
2022-02-15 20:53:31 +0100

    15.2.2022

--- game/main.py
+++ game/main.py
@@ -28,18 +28,20 @@ if __name__ == "__main__":
     #numbers
     skore = 0
     enemies = []
+    end = False
     font = pygame.font.SysFont('Times New Roman', 30)
     sirka = 800
     vyska = 600
-    lod_x = sirka//2
-    lod_y = vyska//2+220
+    ufo_x = sirka//2
+    ufo_y = vyska//2+220
     skore_x = sirka - 100
     skore_y = 33
     enemy_speed = 3
     enemy_count = 0
     enemy_plus = 4
     #image load
-    pozadie = pygame.image.load("vesmir.jpg")
+    pozadie1 = pygame.image.load("vesmir.jpg")
+    pozadie2 = pygame.image.load("Star_wars.jpg")
     ufo = pygame.image.load("lod.png")
     enemy_img = pygame.image.load('nepriatel.png')
     #mask creating(for ship)
@@ -68,22 +70,32 @@ if __name__ == "__main__":
         #game control
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            if lod_x > 2:
-                lod_x -= 8
+            if ufo_x > 2:
+                ufo_x -= 8
         if key[pygame.K_RIGHT]:
-            if lod_x < 714:
-                lod_x += 8
+            if ufo_x < 714:
+                ufo_x += 8
         #draw
-        window.blit(pozadie, (0,0))
-        window.blit(ufo, (lod_x, lod_y))
+        if skore < 50:
+            window.blit(pozadie1, (0,0))
+        else:
+            window.blit(pozadie2, (-350, -200))
+        window.blit(ufo, (ufo_x, ufo_y))
         window.blit(skore_vypis, (skore_x, skore_y))
         #enemy move, score count
-        for enemy in enemies[:]: #copy of the list
-            window.blit(enemy_img, (enemy["x"], enemy["y"]))
-            enemy["y"] += enemy_speed
-            if enemy["y"] > 600:
-                enemies.remove(enemy)
-                skore += 1
+        if not end:
+            for enemy in enemies[:]: #copy of the list
+                window.blit(enemy_img, (enemy["x"], enemy["y"]))
+                enemy["y"] += enemy_speed
+                if enemy["y"] > 600:
+                    enemies.remove(enemy)
+                    skore += 1
+                if colision_check(ufo_mask, enemy["mask"], (ufo_x, ufo_y), (enemy["x"], enemy["y"])):
+                    end = True
+        else:
+            end_font = pygame.font.SysFont("Times New Roman", 50)
+            end_vypis = end_font.render("Game over", True, (255, 10, 10))
+            window.blit(end_vypis, (sirka//2 - 100, vyska//2))
         #end formula
         cas.tick(30)
         pygame.display.update()
